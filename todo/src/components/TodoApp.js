e090a50f67097fbb40c79ae0881b298b8e146b46
albert.oclarit <bbjao143213620>
2016-04-18 11:46:21 +0800

    Initial Commit

---
+++ todo/src/components/TodoApp.js
@@ -0,0 +1,21 @@
+/**
+ * Created by albertoclarit on 4/18/16.
+ */
+import React from 'react';
+
+export default  class TodoApp extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+
+        return (
+            <div>
+                <h1>Welcome to ReactJS</h1>
+            </div>
+        )
+    }
+}
---
+++ todo/src/components/run.js
@@ -0,0 +1,11 @@
+/**
+ * Created by albertoclarit on 4/18/16.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TodoApp from './TodoApp'
+
+
+
+ReactDOM.render(TodoApp, document.getElementById('container'));
