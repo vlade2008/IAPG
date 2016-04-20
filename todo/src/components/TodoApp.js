6db6e90b2002c5bbb14f0881ae0a335450333453
albert.oclarit <bbjao143213620>
2016-04-20 17:32:37 +0800

    Welcome Example

--- todo/src/components/TodoApp.js
+++ todo/src/components/TodoApp.js
@@ -7,15 +7,28 @@ export default  class TodoApp extends React.Component{
 
     constructor(props){
         super(props);
+        this.state={
+            name: 'Student'
+         }
+        // this.onChange = this.onChange.bind(this);
     }
-
-
+   
+  onChange(e){
+     this.setState({
+         name:e.target.value
+     })
+  }
     render(){
 
         return (
             <div>
-                <h1>Welcome to ReactJS Test Atom</h1>
+                <h1>Welcome to ReactJS {this.state.name}</h1>
+                <strong>Enter your name</strong><br/>
+                <form>
+                <input type="text" onChange={this.onChange.bind(this)} />
+                </form>
             </div>
         )
     }
 }
+ 
